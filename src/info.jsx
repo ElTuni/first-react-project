daddc3fb20bbefad6e01a01d212842e55ccf02a5
ElTuni <tomaslucasgoya@gmail.com>
2025-06-24 20:50:30 -0300

    img rendering bug fixed

--- src/info.jsx
+++ src/info.jsx
@@ -1,7 +1,7 @@
 export default function Info() {
     return(
         <section className="section-info container">
-            <img className="profile-pic" src="src/images/profile pic.jpg" alt="profile picture" />
+            <img className="profile-pic" src="./images/profile-pic.jpg" alt="profile picture" />
             <h1>Tomás Goya</h1>
             <p className="job-descrition">Frontend Developer</p>
             <a className="my-website" href="#">tuni.website</a>
