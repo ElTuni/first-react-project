5209ed78fa7f81375899bd3c81f6171fb33a57df
ElTuni <tomaslucasgoya@gmail.com>
2025-06-24 21:06:32 -0300

    try 4 by cursor

--- src/info.jsx
+++ src/info.jsx
@@ -1,7 +1,9 @@
+import profilePic from './images/profile-pic.jpg'
+
 export default function Info() {
     return(
         <section className="section-info container">
-            <img className="profile-pic" src="src/images/profile-pic.jpg" alt="profile picture" />
+            <img className="profile-pic" src={profilePic} alt="profile picture" />
             <h1>Tomás Goya</h1>
             <p className="job-descrition">Frontend Developer</p>
             <a className="my-website" href="#">tuni.website</a>
